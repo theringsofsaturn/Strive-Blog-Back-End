f5f2fb660820c03fea2c4cb632c73342661c2686
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-10-27 20:49:28 +0200

    folders setup

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
-// import listEndpoints from "express-list-endpoints";
+import cors from "cors"
+import listEndpoints from "express-list-endpoints";
 
 const server = express();
 const port = 3001; //server to listen on the port, it is stores into a variable
@@ -16,6 +17,12 @@ server.use(loggerMiddleware)
 //cors and express are middlewares
 server.use(express.json) //this has to be specified BEFORE the routes, otherwise the body will be undefined
 server.use(cors()) //cors connects BE with FE
+
+// routes
+server.use("/authors", authorsRouter)
+server.use("/blogs", blogRouter)
+
+console.table(listEndpoints(server))
 
 //server to listen on the port, it is stores into a variable
 server.listen(port, () => {
