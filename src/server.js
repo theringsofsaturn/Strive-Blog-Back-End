9fd0c0dfe12451078004c643086ea2111923d7bf
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-10-27 20:42:29 +0200

    middlewares

---
+++ src/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+// import listEndpoints from "express-list-endpoints";
+
+const server = express();
+const port = 3001; //server to listen on the port, it is stores into a variable
+
+//*********** MIDDLEWARES ***************************
+const loggerMiddleware = (res, req, next) => {
+  console.log(
+    `REQ METHOD ${req.method} -- REQ URL ${req.url} -- ${new Date()}`
+  );
+  next();
+};
+
+server.use(loggerMiddleware)
+//cors and express are middlewares
+server.use(express.json) //this has to be specified BEFORE the routes, otherwise the body will be undefined
+server.use(cors()) //cors connects BE with FE
+
+//server to listen on the port, it is stores into a variable
+server.listen(port, () => {
+  console.log("listening on port: ", port);
+});
